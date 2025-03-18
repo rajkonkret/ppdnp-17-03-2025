73a7793347c499e0db0dbc0adeeabc6a93906517
rjaniak <rajkonkret660@gmail.com>
2025-03-18 15:37:07 +0100

    [2 12] update warunki.py

--- day_2/warunki.py
+++ day_2/warunki.py
@@ -74,3 +74,63 @@ print(f"Rabat wynosi {rabat}")  # Rabat wynosi 25
 
 rabat = 25 if suma_zam > 100 else 0  # operator warunkowy
 print(f"Rabat wynosi {rabat}")  # Rabat wynosi 25
+
+# zasymulejemy system zbierania logów
+# zmienna bedzie zawierac informację jaki to system
+# email, console, inny
+# gdy log z systemu "console" wyświetlimy napis "Stało się coś strasznego"
+# "email" -> "System email"
+# jesi przyjdzie system "email"
+# to w zmiennej bedzie poziom błedy: error, medium, inny
+# nalezy do listy dodać opis błedu
+alert_system = "email"
+error = "error"
+lista_b = []
+if alert_system == "console":  # == porównanie
+    print("Stało się coś strasznego")
+elif alert_system == "email":
+    print("System email")
+    if error == "error":
+        lista_b.append("Krytyczny")
+    elif error == "medium":
+        lista_b.append("Ostrzeżenie")
+    else:
+        lista_b.append("Inny")
+else:
+    print("Nie znam takiego systemu")
+
+print(lista_b)
+# System email
+# ['Krytyczny']
+
+# napisac program test z ....
+# zadac pytanie
+# pobrac odpowiedz
+# spradzic czy prawidłowo i wyswietlic wynik testu
+punkty = 0
+odp = input("Podaj rok Chrztu Polski")  #
+if odp.strip().casefold() == "966".casefold().strip():
+    punkty += 1  # punkty = punkty + 1
+    print("Dobrze")
+else:
+    print("Idż się pouczyć")
+
+print("Punkty:", punkty)
+# spam += 1    spam = spam + 1
+# spam -= 1    spam = spam - 1
+# spam *= 1    spam = spam * 1
+# spam /= 1    spam = spam / 1
+# spam %= 1    spam = spam % 1
+odp = input("Gdzie się odbył?")
+if odp.strip().casefold() == "Gniezno".strip().casefold():  # Ostrów Lednicki
+    punkty += 1
+    print("Dobrze")
+else:
+    print("Musisz się dalej uczyć")
+print("Razem punktów:", punkty)
+# Podaj rok Chrztu Polski966
+# Dobrze
+# Punkty: 1
+# Gdzie się odbył?Gniezno
+# Dobrze
+# Razem punktów: 2
