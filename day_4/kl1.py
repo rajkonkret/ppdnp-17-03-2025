b79463ef1be416657b6b346e4d01954f04c7b538
rjaniak <rajkonkret660@gmail.com>
2025-03-20 14:49:24 +0100

    [4 15] add kl1

---
+++ day_4/kl1.py
@@ -0,0 +1,26 @@
+# klasa - element programowania obiektowego
+# klasa - szablon, opis, przepis wg którego będą budowane obiekty
+# cechy - pola (zmienne)
+# metody - funkcje
+# klasa musi być zadeklarowana przed użyciem
+# tworzenie obiektu uruchmai metodę __init__ (konstruktor)
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+
+# PascalCase -> PEP8
+# deklaracaja klasy
+class Human:
+    """
+    Kalsa Human opisująca człowieka w Pythonie
+    """
+
+
+# tworzenie obiektu
+cz1 = Human()
+print(Human.__doc__)
+
+print(print.__doc__)
+help(Human)
+
+# pydoc -b uruchomienie serwera z dokumentacją
+# cd .\day_4
