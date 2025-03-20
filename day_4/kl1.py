2ce1bc5ef170b5c5065ef8206ce5d77804b3a250
rjaniak <rajkonkret660@gmail.com>
2025-03-20 15:09:54 +0100

    [4 16] update kl1

--- day_4/kl1.py
+++ day_4/kl1.py
@@ -14,13 +14,59 @@ class Human:
     Kalsa Human opisująca człowieka w Pythonie
     """
 
+    imie = ""
+    wiek = None
+    plec = "k"
+
+    # self - obiekt, który wyołuje metode w klasie
+    def powitanie(self):
+        print(f"Nazywam się {self.imie}")
+
+    # wypisz_wiek
+    def wypisz_wiek(self):
+        print(f"Masz teraz {self.wiek} lat.")
+
 
 # tworzenie obiektu
 cz1 = Human()
-print(Human.__doc__)
+# print(Human.__doc__)
+#
+# print(print.__doc__)
+# help(Human)
 
-print(print.__doc__)
-help(Human)
 
 # pydoc -b uruchomienie serwera z dokumentacją
 # cd .\day_4
+#  pydoc -w kl1 - tworzenie pliku html z dokumentacją
+
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+#
+# None
+# k
+cz1.wiek = 50
+cz1.imie = "Radek"
+cz1.plec = "m"
+
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# Radek
+# 50
+# m
+
+# drugiczłowiek odmiennej płci
+cz2 = Human()
+cz2.imie = "Anna"
+cz2.wiek = 34
+print(cz2.imie)
+print(cz2.wiek)
+print(cz2.plec)
+# Anna
+# 34
+# k
+cz1.powitanie()
+cz2.powitanie()
+# Nazywam się Radek
+# Nazywam się Anna
