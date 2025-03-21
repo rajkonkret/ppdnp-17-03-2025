3bce0c661a8d6388c6c0e50ddf42b90d85e1f1b3
rjaniak <rajkonkret660@gmail.com>
2025-03-21 12:41:19 +0100

    [5 8] add baza2.py

---
+++ day_5/baza_2.py
@@ -0,0 +1,30 @@
+# baza danych - system do zarzadzania danymi
+# bazy relacyjne - sql
+import sqlite3
+
+try:
+    sql_connection = sqlite3.connect('sqlite_python.db')
+    cursor = sql_connection.cursor()
+    print("Baza danych zostala podłaczona")
+
+    query = """
+    CREATE TABLE IF NOT EXISTS developers(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    joining_data DATETIME,
+    salary REAL NOT NULL);
+    """
+
+    # uruchamianie query
+    cursor.execute(query)
+    sql_connection.commit()  # zatwierdzenei zmian
+
+except sqlite3.Error as e:
+    print("Bład otwierania bazy danych", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danych zostalą zamknięta")
+# Baza danych zostala podłaczona
+# Baza danych zostalą zamknięta
